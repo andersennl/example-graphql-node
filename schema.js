14ad13b8b960b0d1412420f552733f0c87ab85d5
Nikolaj Andersen <andersen.nl@gmail.com>
2018-07-05 10:26:12 +0200

    initial commit

---
+++ schema.js
@@ -0,0 +1,80 @@
+const axios = require("axios");
+
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLSchema,
+  GraphQLList,
+  GraphQLNonNull
+} = require("graphql");
+
+const CustomerType = new GraphQLObjectType({
+  name: "Customer",
+  fields: () => ({
+    id: {type: GraphQLString},
+    name: {type: GraphQLString},
+    email: {type: GraphQLString},
+    age: {type: GraphQLInt},
+  })
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    customer: {
+      type: CustomerType,
+      args: {
+        id: {type: GraphQLString}
+      },
+      resolve(parentValue, args) {
+        return axios.get("http://localhost:3000/customers/" + args.id)
+          .then(response => response.data);
+      }
+    },
+    customers: {
+      type: new GraphQLList(CustomerType),
+      resolve() {
+        return axios.get("http://localhost:3000/customers/")
+          .then(response => response.data);
+      }
+    }
+  }
+});
+
+const mutation = new GraphQLObjectType({
+  name: "Mutation",
+  fields: {
+    addCustomer: {
+      type: CustomerType,
+      args: {
+        name: {type: new GraphQLNonNull(GraphQLString)},
+        email: {type: new GraphQLNonNull(GraphQLString)},
+        age: {type: new GraphQLNonNull(GraphQLInt)},
+      },
+      resolve(parentValue, args) {
+        return axios.post("http://localhost:3000/customers", {
+          name: args.name,
+          email: args.email,
+          age: args.age
+        })
+        .then(result => result.data);
+      }
+    },
+    deleteCustomer: {
+      type: CustomerType,
+      args: {
+        id: {type: new GraphQLNonNull(GraphQLString)}
+      },
+      resolve(parentValue, args) {
+        return axios.delete("http://localhost:3000/customers/" + args.id)
+        .then(result => result.data);
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
+  mutation
+});
